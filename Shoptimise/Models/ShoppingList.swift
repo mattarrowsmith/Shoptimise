c163a7ee9b53b29b8fb55c4d208fc415d4c002b2
Matt Arrowsmith <matt.arrowsmith@hotmail.co.uk>
2023-03-28 19:51:12 +0100

    Use session for 'shop' screen lists Modifications to shoppinglist properties to accommodate binding

--- Shoptimise/Models/ShoppingList.swift
+++ Shoptimise/Models/ShoppingList.swift
@@ -9,41 +9,46 @@ import Foundation
 
 struct ShoppingList: Equatable, Hashable, Codable {
     let name: String
-    let recipes: [Recipe]
-    let items: [Item]
+    var recipes: [Recipe]
+    var items: [Item]
     var sharedIngredients: [Recipe.Ingredient] {
-        let firstRecipeIngredients = self.recipes.first?.ingredients ?? [] //list of ingredients
-        let firstRecipeIngredientNames = Set(firstRecipeIngredients.map{ $0.name }) //set of strings ham, bread
-        
-        var ingredientNameLists = [[String]]()
-        
-        self.recipes.forEach { Recipe in
-            let ingredientNames = Recipe.ingredients.map{ $0.name } //list of strings [lettuce, cheese, ham]
-            ingredientNameLists.append(ingredientNames)
-        }
-        
-        let commonElements = ingredientNameLists.reduce((firstRecipeIngredientNames)) { (result, list) in
-            result.intersection(list)
-        }
-        
-        var commonIngredients = [Recipe.Ingredient]()
-        
-        
-        //Improve this once I know more swift!!
-        self.recipes.forEach { ThisRecipe in //use for in instead of foreach
-            ThisRecipe.ingredients.forEach { ThisIngredient in
-                if (commonElements.contains(ThisIngredient.name)) {
-                    
-                    if let row = commonIngredients.firstIndex(where: {$0.name == ThisIngredient.name}) {
-                        commonIngredients[row] = Recipe.Ingredient(name: ThisIngredient.name, price: commonIngredients[row].price + ThisIngredient.price)
-                    }
-                    else {
-                        commonIngredients.append(ThisIngredient)
+        get {
+            let firstRecipeIngredients = self.recipes.first?.ingredients ?? [] //list of ingredients
+            let firstRecipeIngredientNames = Set(firstRecipeIngredients.map{ $0.name }) //set of strings ham, bread
+            
+            var ingredientNameLists = [[String]]()
+            
+            self.recipes.forEach { Recipe in
+                let ingredientNames = Recipe.ingredients.map{ $0.name } //list of strings [lettuce, cheese, ham]
+                ingredientNameLists.append(ingredientNames)
+            }
+            
+            let commonElements = ingredientNameLists.reduce((firstRecipeIngredientNames)) { (result, list) in
+                result.intersection(list)
+            }
+            
+            var commonIngredients = [Recipe.Ingredient]()
+            
+            
+            //Improve this once I know more swift!!
+            self.recipes.forEach { ThisRecipe in //use for in instead of foreach
+                ThisRecipe.ingredients.forEach { ThisIngredient in
+                    if (commonElements.contains(ThisIngredient.name)) {
+                        
+                        if let row = commonIngredients.firstIndex(where: {$0.name == ThisIngredient.name}) {
+                            commonIngredients[row] = Recipe.Ingredient(name: ThisIngredient.name, price: commonIngredients[row].price + ThisIngredient.price)
+                        }
+                        else {
+                            commonIngredients.append(ThisIngredient)
+                        }
                     }
                 }
             }
+            return commonIngredients
         }
-        return commonIngredients
+        set {
+            
+        }
     }
 }
 
--- Shoptimise/Views/Shop/ShopViewModel.swift
+++ Shoptimise/Views/Shop/ShopViewModel.swift
@@ -15,58 +15,25 @@ extension ShopView{
         @Published var shoppingLists: [ShoppingList]
         @Published var selectedShoppingList: ShoppingList
         
-        init(shoppingList: ShoppingList) {
+        init(shoppingLists: [ShoppingList]) {
             self.title = "Shop"
-            self.shoppingLists = [shoppingList]
-            self.selectedShoppingList = ShopViewModel.createDummyList(name: "Englando")
+            self.shoppingLists = shoppingLists
+            self.selectedShoppingList = shoppingLists.first ?? ShopView.PreviewHelper().shoppingList
         }
-        
-        init() {
-            self.title = "Shop"
-            self.shoppingLists = [ShopViewModel.createDummyList(name: "American"), ShopViewModel.createDummyListMexican(name: "Mexican")]
-            self.selectedShoppingList = ShopViewModel.createDummyList(name: "Canadian")
-        }
-        
-        static func createDummyList(name: String) -> ShoppingList {
-            let ingredient = Recipe.Ingredient(name: "Potato", price: 1.5)
-            let ingredient2 = Recipe.Ingredient(name: "Oil", price: 1)
+    }
+}
+
+
+extension ShopView {
+    struct PreviewHelper {
+        var shoppingList: ShoppingList
+    //TODO: A better way to handle the default no list state
+        init(){
+            let ingredient = Recipe.Ingredient(name: "{ingredient}", price: 1)
+            let item = ShoppingList.Item(name: "{item}", price: 1)
             
-            let ingredient3 = Recipe.Ingredient(name: "Lettuce", price: 0.80)
-            let ingredient4 = Recipe.Ingredient(name: "Patty", price: 2.50)
-            let ingredient5 = Recipe.Ingredient(name: "Bun", price: 1.10)
-            let ingredient6 = Recipe.Ingredient(name: "Potato", price: 0.30)
-            
-            let item = ShoppingList.Item(name: "Spoon", price: 1)
-            let item2 = ShoppingList.Item(name: "Knife", price: 1)
-            let item3 = ShoppingList.Item(name: "Fork", price: 1)
-            
-            let recipe = Recipe(name: "Chips", ingredients: [ingredient, ingredient2], imageUrl: URL(string: "https://picsum.photos/300") ?? URL(string: "https://picsum.photos/300")!)
-            let recipe2 = Recipe(name: "Burgers", ingredients: [ingredient3, ingredient4, ingredient5, ingredient6], imageUrl: URL(string: "https://picsum.photos/300") ?? URL(string: "https://picsum.photos/300")!)
-            let shoppingListDummy = ShoppingList(name: name, recipes: [recipe, recipe2], items: [item, item2, item3])
-            
-            return shoppingListDummy
-        }
-        
-        static func createDummyListMexican(name: String) -> ShoppingList {
-            let ingredient = Recipe.Ingredient(name: "Tortilla", price: 1.5)
-            let ingredient2 = Recipe.Ingredient(name: "Oil", price: 1)
-            let ingredient3 = Recipe.Ingredient(name: "Cheese", price: 1)
-            let ingredient4 = Recipe.Ingredient(name: "Beef", price: 0.80)
-            let ingredient5 = Recipe.Ingredient(name: "Lettuce", price: 2.50)
-            let ingredient6 = Recipe.Ingredient(name: "Sour Cream", price: 1.10)
-            let ingredient7 = Recipe.Ingredient(name: "Salsa", price: 0.30)
-            let ingredient8 = Recipe.Ingredient(name: "Pepper", price: 0.30)
-            let ingredient9 = Recipe.Ingredient(name: "Chicken", price: 0.30)
-            
-            let item = ShoppingList.Item(name: "Dishwasher Tablets", price: 1)
-            let item2 = ShoppingList.Item(name: "Kitchen Roll", price: 1)
-            let item3 = ShoppingList.Item(name: "Sombrero", price: 1)
-            
-            let recipe = Recipe(name: "Tacos", ingredients: [ingredient, ingredient2, ingredient3, ingredient4, ingredient5, ingredient6, ingredient7], imageUrl: URL(string: "https://picsum.photos/300") ?? URL(string: "https://picsum.photos/300")!)
-            let recipe2 = Recipe(name: "Fajitas", ingredients: [ingredient, ingredient3, ingredient5, ingredient6, ingredient7, ingredient8, ingredient9], imageUrl: URL(string: "https://picsum.photos/300") ?? URL(string: "https://picsum.photos/300")!)
-            let shoppingListDummy = ShoppingList(name: name, recipes: [recipe, recipe2], items: [item, item2, item3])
-            
-            return shoppingListDummy
+            let recipe = Recipe(name: "{recipe}", ingredients: [ingredient], imageUrl: URL(string: "https://picsum.photos/300")!)
+            self.shoppingList = ShoppingList(name: "No list found", recipes: [recipe], items: [item])
         }
     }
 }
